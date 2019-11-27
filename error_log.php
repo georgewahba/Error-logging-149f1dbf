a4efdd507d0fdaa89254572d59c2ad7834303f13
GeorgeWahba <george-wahba@hotmail.com>
2019-11-27 12:04:46 +0100

    Uitwerking van de exercise

---
+++ error_log.php
@@ -0,0 +1,23 @@
+<!DOCTYPE html>
+<html>
+  <head>
+    <meta charset="utf-8">
+    <title></title>
+  </head>
+  <body>
+    <?php set_error_handler(function(){
+      throw new Exception("is fout");
+    });
+    try {
+      echo 1234567890 / 0;
+    } catch (Exception $e) {
+    echo "<h1>er is wat fout gegaan</h1>";
+    error_log($e, 3, "error.log");
+    }
+    restore_error_handler();
+
+     ?>
+
+
+  </body>
+</html>
